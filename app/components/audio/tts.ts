7ecf480f12b9527eda83d89fcfb8218ef689bb86
John <john.tan-aristy@csedge.org>
2024-12-01 00:53:54 -0600

    not commitng api keys

--- app/components/audio/tts.ts
+++ app/components/audio/tts.ts
@@ -1,11 +1,8 @@
 //@ts-ignore
 "use server";
 
-const EDENAI_URL =
-  "https://api.edenai.run/v2/workflow/61cb48f5-3ae4-4f53-8a1f-5a3e6b6abf85/execution/";
-const EDENAI_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTFmNTE3NzEtYjdjNi00ODQ0LWFmMzQtYzA2MzA5NTFhOGEwIiwidHlwZSI6ImFwaV90b2tlbiJ9.-6xHPx0cGn4CtB9f-UWQBaoWfjvCVQk2gWzu_kKpQkg";
-
+const EDENAI_URL = process.env.EDENAI_URL;
+const EDENAI_API_KEY = process.env.EDENAI_API_KEY
 export default async function TTS(sentence: string) {
   
   const payload = { text: sentence, input_language: "en" };
